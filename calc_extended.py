c3ce45a4409e40527460dedf7922584d0e5e83ab
Creatize <108670738+Creatize@users.noreply.github.com>
2023-03-09 08:51:27 +0300

    Update calc_extended.py
    yt evt. hf,jnfnm c ubn[f,jv
    --------------
    не умею работать с гитхабом

--- calc_extended.py
+++ calc_extended.py
@@ -14,8 +14,8 @@ from math import sqrt, factorial, sin, cos, tan, radians
 from sympy import subfactorial 
   
 class Calcmod(loader.Module): 
-    strings = {"name": "Extended calc", "err_int": "Only integral values", "_cls_doc": " Calculator with many functions. Only .e is better", "err_txt": "<b>[cl]</b> Error in a case"}
-    strings_ru = {"err_int": "Только целые значения", "_cls_doc": " Калькулятор со множеством функций. Лучше только .e", "err_txt": "<b>[cl]</b> Ошибка в примере"}
+    strings = {"name": "Extended calc", "_cls_doc": " Calculator with many functions. Only .e is better", "err_txt": "<b>[Calc]</b> Error in a case"}
+    strings_ru = {"_cls_doc": " Калькулятор со множеством функций. Лучше только .e", "err_txt": "<b>[Calc]</b> Ошибка в примере"}
     
  
     async def clcmd(self, message): 
@@ -32,12 +32,15 @@ class Calcmod(loader.Module):
         try:
             await utils.answer(message, f"{args}! = <code>{round(factorial(int(args)), 5)}</code>") 
         except:
-            await utils.answer(message, self.strings("err_int"))
+            await utils.answer(message, self.strings("err_txt"))
  
     async def sqrtcmd(self, message): 
         """- вычисляет квадратный корень числа""" 
-        args = utils.get_args_raw(message) 
-        await utils.answer(message, f"√{args} = <code>{round(sqrt(float(args)), 5)}</code>") 
+        args = utils.get_args_raw(message)
+        try:
+            await utils.answer(message, f"√{args} = <code>{round(sqrt(float(args)), 5)}</code>")
+        except:
+            await utils.answer(message, self.strings("err_txt"))
  
     async def sfctcmd(self, message): 
         """- вычисляет субфакториал числа""" 
@@ -45,19 +48,28 @@ class Calcmod(loader.Module):
         try:
             await utils.answer(message, f"!{args} = <code>{round(subfactorial(int(args)), 5)}</code>")
         except:
-            await utils.answer(message, self.strings("err_int"))
+            await utils.answer(message, self.strings("err_txt"))
  
     async def coscmd(self, message): 
         """- вычисляет косинус числа""" 
-        args = utils.get_args_raw(message) 
-        await utils.answer(message, f"cos({args}) = <code>{round(cos(radians(float(args))), 5)}</code>") 
+        args = utils.get_args_raw(message)
+        try:
+            await utils.answer(message, f"cos({args}) = <code>{round(cos(radians(float(args))), 5)}</code>")
+        except:
+            await utils.answer(message, self.strings("err_txt"))
  
     async def sincmd(self, message): 
         """- вычисляет синус числа""" 
-        args = utils.get_args_raw(message) 
-        await utils.answer(message, f"sin({args}) = <code>{round(sin(radians(float(args))), 5)}</code>") 
+        args = utils.get_args_raw(message)
+        try:
+            await utils.answer(message, f"sin({args}) = <code>{round(sin(radians(float(args))), 5)}</code>")
+        except:
+            await utils.answer(message, self.strings("err_txt"))
  
     async def tancmd(self, message): 
         """- вычисляет тангенс числа""" 
-        args = utils.get_args_raw(message) 
-        await utils.answer(message, f"tg({args}) = <code>{round(tan(radians(float(args))), 5)}</code>")
+        args = utils.get_args_raw(message)
+        try:
+            await utils.answer(message, f"tg({args}) = <code>{round(tan(radians(float(args))), 5)}</code>")
+        except:
+            await utils.answer(message, self.strings("err_txt"))
